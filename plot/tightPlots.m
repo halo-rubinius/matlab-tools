ba7723f0cc5889292bc12d4d2cc5de08116acdbe
Libin <formylady90@hotmail.com>
2020-12-25 14:23:32 +0800

    添加紧凑型布局预设

--- plot/tightPlots.m
+++ plot/tightPlots.m
@@ -10,6 +10,7 @@ addOptional(p, 'gap', [1.6, 1.8], @(x) isnumeric(x));
 addOptional(p, 'xedge', [1.3, 0.1], @(x) isnumeric(x));
 addOptional(p, 'yedge', [1.0, 0.5], @(x) isnumeric(x));
 addOptional(p, 'units', 'centimeters', @(x) ischar(x));
+addParameter(p, 'preset', '', @(x) ischar(x));
 parse(p, varargin{:});
 
 nrow = p.Results.nrow;
@@ -42,6 +43,11 @@ end
 
 units = p.Results.units;
 
+preset = p.Results.preset;
+if strcmp(preset, 'compact')
+    gap = [0.2, 0.2];
+end
+
 axw = (figwidth - sum(xedge) - (ncol - 1) * gap(1)) / ncol;
 axh = axw * AR;
 
@@ -64,6 +70,14 @@ for irow = 1:nrow
         axs(irow, jcol) = axes;
         set(gca, 'Units', units, 'Position', [px, py, axw, axh]);
         px = px + axw + gap(1);
+        if strcmp(preset, 'compact')
+            if irow < nrow
+                set(gca, 'xticklabel', '');
+            end
+            if jcol > 1
+                set(gca, 'yticklabel', '');
+            end
+        end
     end
     py = py - axh - gap(2);
 end
